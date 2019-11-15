d35da2b7a7ec1632527bedd8d2a7b21471152ce3
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2019-11-15 08:24:53 -0500

    Implementando exemplo basico em java

---
+++ java/src/main/java/br/com/programaai/avro/Application.java
@@ -0,0 +1,9 @@
+package br.com.programaai.avro;
+
+import java.util.Arrays;
+
+public class Application {
+  public static void main(String[] args) {
+    System.out.println(String.format("%s", Arrays.asList(args)));
+  }
+}
